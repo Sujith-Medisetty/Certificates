d7c58134239cf68c64b8174c0c9ae4314d49c051
Sujith-Medisetty <70951136+Sujith-Medisetty@users.noreply.github.com>
2021-07-27 11:10:15 +0530

    commit-done

---
+++ Batch1-Maven-Servlet1/src/main/java/com/app/Servlet2.java
@@ -0,0 +1,42 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		response.setContentType("text/html");
+		
+		ServletConfig config=getServletConfig();
+		
+		PrintWriter out=response.getWriter();
+		out.print("<h1>"+config.getInitParameter("servlet-name")+"</h1>");
+		out.print("<h3>"+config.getInitParameter("query-2")+"</h3>");
+		
+	}
+
+}
